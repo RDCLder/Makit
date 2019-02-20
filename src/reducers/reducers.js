e41fb43379d7b2a16eb51b946e0b67cc3e248425
RDCLder <riy2@case.edu>
2019-02-20 10:41:57 -0600

    initialize

---
+++ src/reducers/reducers.js
@@ -0,0 +1,25 @@
+
+const reducer = (state, action) => {
+
+    if (state === undefined) {
+
+    }
+
+    switch (action.type) {
+        case "":
+            return {
+
+            };
+        case "":
+            return {
+
+            };
+        case "":
+            return {
+
+            };
+        default:
+            return state;
+    }
+
+}
